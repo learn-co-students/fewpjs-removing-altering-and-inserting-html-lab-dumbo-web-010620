d20e46d7f52395cd1dc58c6e0ec192995b119de9
Gregory Diaz <gregdiaz8624@yahoo.com>
2020-02-21 15:48:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let element = document.createElement('div')
+
+let main = document.querySelector("#main")
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+
+newHeader.textContent = "YOUR-NAME is the champion"
